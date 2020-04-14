bf2438c77ef165d86e4acd1eb2e72f03463e3c79
kimberlyt <jkanjels@gmail.com>
2020-04-13 19:48:56 -0500

    'info3180-project1'

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ UPLOAD_FOLDER = './app/static/uploads'
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "h24rfchHUW9iHBu00ih8HHJ9gfes8rd6RYFKDODRTC76R"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://rhpdiandwrgjnj:aae5cd2e861ef24cac08418b6d756f6da365684aeaa62ac3c09b03c9f1ed92e8@ec2-3-223-21-106.compute-1.amazonaws.com:5432/d5ilnvsieien5g"
+# postgresql://project1:project1@localhost/project1
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # added just to suppress a warning
 
 db = SQLAlchemy(app)
